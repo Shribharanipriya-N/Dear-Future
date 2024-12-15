f2381d587ada13d60db3b02dd55570b94701a66b
Shribharanipriya-N <shribharanipriya2004@gmail.com>
2024-12-15 07:58:25 +0530

    Email function completed

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,45 @@ const cors=require('cors');
 require('dotenv').config();
 const UserRoutes=require('./routes/UserRoute');
 const LetterRoutes=require('./routes/LetterRoute');
+const nodemailer=require('nodemailer');
+
 
 const app=express();
 app.use(bodyparser.json());
 app.use(cors());
 
-mongoose.connect(process.env.Mongo_url).then(()=>{
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'dearfuture2k24@gmail.com',
+    pass: 'twor aywo imxg vtfr',
+  },
+});
+async function sendMail(){
+const mailOptions = {
+                  from: 'dearfuture2k24@gmail.com',
+                  to: 'dearfuture2k24@gmail.com',
+                  subject: 'Sample Letter',
+                  text: `Dear ,This is a reminder for your task (DearFuture)`
+                };
+    
+                transporter.sendMail(mailOptions, function (error, info) {
+                  if (error) {
+                    console.log(`Error sending email for task :`, error);
+                  } else {
+                    console.log(`Email sent for task`, info.response);
+                  }
+                });
+            }
+    mongoose.connect(process.env.Mongo_url).then(()=>{
     console.log("MongoDB connected...");
 }).catch(e=>{
     console.log("Can't connect MongoDB...",e);
 })
+sendMail();
+
+
+
 
 app.use('/',UserRoutes);
 app.use('/',LetterRoutes);
